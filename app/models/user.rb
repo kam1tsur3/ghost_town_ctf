414162559759d877664aa5fe7e0f68678f20cb48
kam1tsur3 <kam1tsur3@gmail.com>
2021-01-12 21:01:21 +0900

    Implement cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     # uniqueness: 一意性
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50}
     validates :email, presence: true, length: { maximum: 255},
@@ -16,5 +17,24 @@ class User < ApplicationRecord
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
             BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        # remember_digest はself.remember_digestと同義であり、ActiveRecodeの場合このような記法でDBから取り出すことができる
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 end
