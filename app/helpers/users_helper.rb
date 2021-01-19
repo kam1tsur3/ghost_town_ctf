a4a1fb31268d2eccb795682778892eb91489e347
kam1tsur3 <kam1tsur3@gmail.com>
2021-01-20 00:59:58 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
     def account_img(user)
-        image_tag("default_account.png", alt: user.name, class:"account_img", width: "200px")
+        image_tag("default_account.png", alt: user.name, class:"account_img", width: "100px")
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "kam1tsur3",
+  email: "kam1tsur3@kam.kam",
+  password: "password",
+  password_confirmation: "password",
+  admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@example.com"
+  password = "password"
+  User.create!(name: name, email: email, password: password, password_confirmation: password)
+end
